c39c3e8a863a263ad6a301b0eac0821f61307736
delvago <david96vg@gmail.com>
2026-04-10 13:36:47 -0500

    Se añade el borrador de las lineas de código en el main

--- tamagotchi_botanico/main/main.c
+++ tamagotchi_botanico/main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    printf("Iniciando el cerebro del Tamagotchi...\n");
 
+    while (1) {
+        // Aquí integraremos la lectura del sensor
+        printf("Sistema en línea.\n");
+        
+        // Pausa de 1000 milisegundos (1 segundo) para ceder control al RTOS
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
+    }
 }
