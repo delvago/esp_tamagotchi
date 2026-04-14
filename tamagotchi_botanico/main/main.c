9661b0d896bc23f0c338b66635f07f652a334471
delvago <david96vg@gmail.com>
2026-04-14 11:08:48 -0500

    Se modifica el main para que realice la lectura del pin 10 del ESP32

--- tamagotchi_botanico/main/main.c
+++ tamagotchi_botanico/main/main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_adc/adc_oneshot.h"
 
 void app_main(void)
 {
-    printf("Iniciando el cerebro del Tamagotchi...\n");
+    // Configuración del ADC
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config = {
+        .unit_id = ADC_UNIT_1,
+    };
+    adc_oneshot_new_unit(&init_config, &adc1_handle);
+
+    // Configurar el canal 
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT, // Resolución de 12 bits
+        .atten = ADC_ATTEN_DB_12, // Rango Completo de voltaje (0-3.3V)
+    };
+    adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_9, &config);
+
+    printf("Cerebro iniciado. ADC configurado en GPIO10. \n");
+
+    int lectura_raw = 0;
 
     while (1) {
-        // Aquí integraremos la lectura del sensor
-        printf("Sistema en línea.\n");
-        
-        // Pausa de 1000 milisegundos (1 segundo) para ceder control al RTOS
-        vTaskDelay(1000 / portTICK_PERIOD_MS); 
+        // Realizar la lectura
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_9, &lectura_raw);
+
+        printf("Humedad Raw: %d\n", lectura_raw);
+
+        // Pausa de 5 segundos
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
