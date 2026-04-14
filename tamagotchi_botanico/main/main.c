21efc9623d70d2007fb7aa02bc26698235c6ce25
delvago <david96vg@gmail.com>
2026-04-14 11:20:48 -0500

    Se perfecciona el código para manejo de errores y se aplica el estándar de lectura de ESP

--- tamagotchi_botanico/main/main.c
+++ tamagotchi_botanico/main/main.c
@@ -2,6 +2,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_adc/adc_oneshot.h"
+#include "esp_log.h"
+
+static const char *TAG = "BOTANICO_ADC";
 
 void app_main(void)
 {
@@ -10,7 +13,8 @@ void app_main(void)
     adc_oneshot_unit_init_cfg_t init_config = {
         .unit_id = ADC_UNIT_1,
     };
-    adc_oneshot_new_unit(&init_config, &adc1_handle);
+    // Es buena práctica verificar el retorno de las funciones de configuración
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc1_handle));
 
     // Configurar el canal 
     adc_oneshot_chan_cfg_t config = {
@@ -19,15 +23,18 @@ void app_main(void)
     };
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_9, &config);
 
-    printf("Cerebro iniciado. ADC configurado en GPIO10. \n");
+    ESP_LOGI(TAG, "Cerebro iniciado. ADC configurado en GPIO10.");
 
     int lectura_raw = 0;
 
     while (1) {
         // Realizar la lectura
-        adc_oneshot_read(adc1_handle, ADC_CHANNEL_9, &lectura_raw);
-
-        printf("Humedad Raw: %d\n", lectura_raw);
+        esp_err_t ret = adc_oneshot_read(adc1_handle, ADC_CHANNEL_9, &lectura_raw);
+        if (ret == ESP_OK) {
+            ESP_LOGI(TAG, "Humedad Raw: %d", lectura_raw);
+        } else {
+            ESP_LOGE(TAG, "Error al leer ADC: %s", esp_err_to_name(ret));
+        }
 
         // Pausa de 5 segundos
         vTaskDelay(5000 / portTICK_PERIOD_MS);
